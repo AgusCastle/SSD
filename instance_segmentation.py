d202835324fbd20e138086ab04a992c0c73bf325
agusCastle <agu13xd@hotmail.com>
2022-05-30 18:39:06 -0500

    uPDATE repo

--- instance_segmentation.py
+++ instance_segmentation.py
@@ -17,6 +17,45 @@ def get_model_instance_segmentation(num_classes):
 
     #replace the pre-trained head with a new one
     anchors = model.anchor_generator.num_anchors_per_location()
+
+    #SSD
+    model.head.classification_head = SSDClassificationHead(in_features, anchors, num_classes)
+
+
+    print(model)
+    return model
+
+def get_model_instance_segmentation_transfer(num_classes):
+    # load an instance segmentation model pre-trained on COCO
+    
+    model = torchvision.models.detection.ssd300_vgg16(pretrained=True)
+    #get number of input features for the classifier
+
+    for p in model.parameters():
+        p.requires_grad = False
+
+    in_features = det_utils.retrieve_out_channels(model.backbone, (300, 300))
+
+    #replace the pre-trained head with a new one
+    anchors = model.anchor_generator.num_anchors_per_location()
+    
+    #SSD
+    model.head.classification_head = SSDClassificationHead(in_features, anchors, num_classes)
+
+
+    print(model)
+    return model
+
+def get_model_instance_segmentation_fromS(num_classes):
+    # load an instance segmentation model pre-trained on COCO
+    
+    model = torchvision.models.detection.ssd300_vgg16(pretrained=False)
+    #get number of input features for the classifier
+
+    in_features = det_utils.retrieve_out_channels(model.backbone, (300, 300))
+
+    #replace the pre-trained head with a new one
+    anchors = model.anchor_generator.num_anchors_per_location()
     
     #SSDlite
     #norm_layer = partial(nn.BatchNorm2d, eps= 0.001, momentum=0.03)
@@ -27,4 +66,60 @@ def get_model_instance_segmentation(num_classes):
 
 
     print(model)
-    return model    
+    return model
+
+#
+#   SSD LITE 
+#
+#
+
+def get_model_instance_segmentation_SSD_LITE(num_classes):
+    # load an instance segmentation model pre-trained on COCO
+    
+    model = torchvision.models.detection.ssdlite320_mobilenet_v3_large(pretrained=True)
+    
+    #get number of input features for the classifier
+    in_features = det_utils.retrieve_out_channels(model.backbone, (300, 300))
+    anchors = model.anchor_generator.num_anchors_per_location()
+    norm_layer = partial(nn.BatchNorm2d, eps= 0.001, momentum=0.03)
+    model.head.classification_head = SSDLiteClassificationHead(in_features, anchors, num_classes, norm_layer)
+
+
+    print(model)
+    return model
+
+def get_model_instance_segmentation_transfer_lite(num_classes):
+    # load an instance segmentation model pre-trained on COCO
+    
+    model = torchvision.models.detection.ssdlite320_mobilenet_v3_large(pretrained=True)
+    #get number of input features for the classifier
+
+    for p in model.parameters():
+        p.requires_grad = False
+
+    in_features = det_utils.retrieve_out_channels(model.backbone, (300, 300))
+
+    #replace the pre-trained head with a new one
+    anchors = model.anchor_generator.num_anchors_per_location()
+    norm_layer = partial(nn.BatchNorm2d, eps= 0.001, momentum=0.03)
+    model.head.classification_head = SSDLiteClassificationHead(in_features, anchors, num_classes, norm_layer)
+
+    print(model)
+    return model
+
+def get_model_instance_segmentation_fromS_lite(num_classes):
+    # load an instance segmentation model pre-trained on COCO
+    
+    model = torchvision.models.detection.ssd300_vgg16(pretrained=False)
+    #get number of input features for the classifier
+
+    in_features = det_utils.retrieve_out_channels(model.backbone, (300, 300))
+
+    #replace the pre-trained head with a new one
+    anchors = model.anchor_generator.num_anchors_per_location()
+    norm_layer = partial(nn.BatchNorm2d, eps= 0.001, momentum=0.03)
+    model.head.classification_head = SSDLiteClassificationHead(in_features, anchors, num_classes, norm_layer)
+
+
+    print(model)
+    return model
